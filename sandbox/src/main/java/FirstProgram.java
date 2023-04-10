25ed04cc17c3ba20f0441852373ac4f7df5cc2a9
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-04-11 01:23:47 +0300

    Выполнены 3 части задачи

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -2,5 +2,12 @@ public class FirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    Point p1 = new Point(7,4);
+    Point p2 = new Point(5,2);
+    System.out.println("Расстояние между двумя точками = " + distance(p1,p2));
+  }
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
   }
 }
