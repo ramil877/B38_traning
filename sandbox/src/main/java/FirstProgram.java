f0c669827a830ed8ccb1ba9b903860b0b82817c0
Ramil Bikkinin <ramilbik211090@gmail.com>
2023-04-11 19:53:46 +0300

    Выполнено задание 2

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -5,9 +5,7 @@ public class FirstProgram {
 
     Point p1 = new Point(7,4);
     Point p2 = new Point(5,2);
-    System.out.println("Расстояние между двумя точками = " + distance(p1,p2));
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
   }
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-  }
+
 }
